b0c816aef9eadd46efbdbf35384053e9f6206166
Andrew Plummer <plummer574@gmail.com>
2015-02-17 21:24:33 +0000

    Add initial tests

--- tests.py
+++ tests.py
@@ -1,5 +1,35 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from mainstay.test_utils import MainstayTest
 
-class KanbanTestCase(TestCase):
-    def test_simple(self):
-        self.assertTrue(True)
+from .models import Task
+
+
+class KanbanTestCase(MainstayTest):
+    fixtures = MainstayTest.fixtures + ['kanban_tasks']
+    def test_user_loaded(self):
+        user = User.objects.get()
+        self.assertEqual(user.username, 'admin')
+        self.assertEqual(user.is_superuser, True)
+
+    def test_pages_loaded(self):
+        self.assertEqual(Task.objects.count(), 3)
+        task_names = {t.name for t in Task.objects.all()}
+        self.assertEqual(task_names, {'TaskNew', 'TaskStarted', 'TaskCompleted'})
+
+    def test_main_page(self):
+        self.login()
+        r = self.client.get('/kanban')
+        self.assertEqual(len(r.context['todo']), 1)
+        self.assertEqual(len(r.context['in_progress']), 1)
+        self.assertEqual(len(r.context['completed']), 1)
+
+    def test_add_task(self):
+        self.login()
+        post = {'name': 'MyTask',
+                'content': 'MyContent'}
+        r = self.client.post('/kanban/add/', post, follow=True)
+        self.assertRedirects(r, '/kanban')
+        self.assertEqual(len(r.context['todo']), 2)
+        self.assertEqual(len(r.context['in_progress']), 1)
+        self.assertEqual(len(r.context['completed']), 1)
